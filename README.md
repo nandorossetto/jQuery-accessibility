90a035cd97c77d0cd057a591f75c6c7333579d96
nandorossetto <hello@nandorossetto.com>
2011-12-29 16:48:13 -0200

    Plugin jQuery Acessibility

---
+++ README.md
@@ -0,0 +1,11 @@
+#How to use:
+* First, include the jquery.acessibility.js script inside your page, then:
+* You just call the plugin, like this: $("#container").fontSize(); and it works with the default settings!
+* If you don,t want to use the custom value, you can change it, just like this: $("#container").fontSize({"minSize" : "5", "maxSize" : "22"});
+
+#Default options:
+* size : 12
+* target : "p"
+* maxSize : 17
+* minSize : 9
+* speed : 100
---
+++ js/jquery.accessibility.js
@@ -0,0 +1,50 @@
+/*
+jQuery Acessibility Plugin
+version: 0.0.1
+@requires jQuery 1.6 or later
+@homepage https://github.com/nandorossetto/jQuery-accessibility
+
+Licensed under the MIT:
+	http://www.opensource.org/licenses/mit-license.php
+	Copyright nandorossetto
+
+Usage:
+   $("#container").fontSize();
+
+Default options:
+	size    : 12,
+	target  : "p",
+	maxSize : 17,
+	minSize : 9,
+	speed   : 100
+*/
+
+(function($){
+    $.fn.fontSize = function(settings){
+		var settings = $.extend({
+			size    : 12,
+			target  : "p",
+			maxSize : 17,
+			minSize : 9,
+			speed   : 100
+		}, settings);
+
+		$(this).prepend("<div id='font-size'><a href='javascript:;;' class='font-bigger'>A+</a> <a href='javascript:;;' class='font-smaller'>A-</a></div>");
+
+		//make a bigger font
+		$(".font-bigger").click(function(){
+			if(settings.size < settings.maxSize){
+				settings.size += 1;
+				$(settings.target).animate({fontSize: settings.size}, settings.speed);
+			}return false;
+		});
+
+		//make a smaller font
+		$(".font-smaller").click(function(){
+			if(settings.size > settings.minSize){
+				settings.size -= 1;
+				$(settings.target).animate({fontSize: settings.size}, settings.speed);
+			}return false;
+		});
+    }
+})(jQuery);
